80006be4dec058951a4eadaddb63b0a1d0183786
torian <torianyap@gmail.com>
2020-09-25 10:18:21 +0700

    'addTrivia'

--- game.js
+++ game.js
@@ -4,70 +4,80 @@ let questionList = [
         answer1: '2',
         answer2: '3',
 
-        correct: 2
+        correct: 2,
+        trivia: 'this is a basic math problem'
     },
     {
         question: "The Marina Trench is located in",
         answer1: 'Eastern Pacific Ocean',
         answer2: 'Western Pacific Ocean',
 
-        correct: 'Western Pacific Ocean'
+        correct: 'Western Pacific Ocean',
+        trivia: 'The Marina Trench is the deepest oceanic trench in the whole world'
     },
     {
         question: "Square root of 169",
         answer1: "14",
         answer2: "13",
 
-        correct: 2
+        correct: 2,
+        trivia: 'easy peasy lemon squeezy'
     },
     {
         question: "Which one of this that isn't an element",
         answer1: "Einsteinium",
         answer2: "Baron",
 
-        correct: 2
+        correct: 2,
+        trivia: 'One of them has the atomic number of 99'
     },
     {
         question: "The creator of dynamite",
         answer1: "Alfred Nobel",
         answer2: "Ludwig Nobel",
 
-        correct: 1
+        correct: 1,
+        trivia: 'The Nobel Prize was named afther him'
     },
     {
         question: "Where is Serengeti located",
         answer1: "India",
         answer2: "Africa",
 
-        correct: 2
+        correct: 2,
+        trivia: 'Serengeti means endless plains in Maasai language'
     },
     {
         question: "Where was Confucius from?",
         answer1: "Tibet",
         answer2: "China",
 
-        correct: 2
+        correct: 2,
+        trivia: 'Confucianism is a philosophical doctrine created by him'
     },
     {
         question: "I only have a tail and a head",
         answer1: "Coin",
         answer2: "Clock",
 
-        correct: 1
+        correct: 1,
+        trivia: 'Think very carefully'
     },
     {
         question: "Smallest country in the world",
         answer1: "Hawaii",
         answer2: "Vatican City",
 
-        correct: 2
+        correct: 2,
+        trivia: 'This country only has an area of 49 ha'
     },
     {
         question: "Decode me: 8181",
         answer1: "Haha",
         answer2: "Hehe",
 
-        correct: 1
+        correct: 1,
+        trivia: 'Alphabet.'
     },
 ]
 
@@ -80,6 +90,7 @@ let questionCounter = 0
 let score = 0
 let availableQuestion = []
 let maxQuestion = 10
+let trivia = document.getElementById('isi')
 
 function startGame (){
     questionCounter = 0
@@ -105,6 +116,7 @@ function getNextQuestion (){
         const number = choice.dataset['number']
         choice.innerText = currentQuestion['answer' + number]
     });
+    trivia.innerText = currentQuestion['trivia']
     availableQuestion.splice(questionIndex,1)
     acceptingAnswer = true
 }
